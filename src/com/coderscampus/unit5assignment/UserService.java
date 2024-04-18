065a241d330e585edcd02e454917a521b9906f05
theTurnedPage <158675598+theTurnedPage@users.noreply.github.com>
2024-04-18 13:27:17 -0500

    Still troubleshooting
    stringArray3 is still only receiving values into the 3 first indexes, and the rest are null

--- src/com/coderscampus/unit5assignment/UserService.java
+++ src/com/coderscampus/unit5assignment/UserService.java
@@ -7,17 +7,17 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class UserService {
-		
+	//========================================================
 	public static void appUsernamePrompt() {
 		System.out.println("---------");
 		System.out.println("Username:\n >");		
 	}
-	
+	//========================================================
 	public static void appPasswordPrompt() {
 		System.out.println("---------");
 		System.out.println("Password:\n >");		
 	}
-
+	//========================================================
 	public User createUser (String username, String password, String name) {
 		//create new instance of object 
 		User newUser = new User();
@@ -28,19 +28,17 @@ public class UserService {
 		//return instance of object
 		return newUser;
 	}	
-	
+	//========================================================
 	public static void appInput() {
 		
 		//initialize objects
 		String[] stringArray = new String[4]; //set to 4 since data.txt has 4 lines
-		String[] stringArray2 = new String[12];
-		
-		User[] userArray = new User[12]; //take as string then convert to object User
+		String[] stringArray2 = new String[12];		
+		User[] userArray = new User[12];
 		User user = new User();
+		UserService userService = new UserService();		
 		Scanner inputScanner = new Scanner(System.in);		
 		BufferedReader fileReader = null;
-		UserService userService = new UserService();
-
 		
 		//=================BEGIN input from USER====================
 		//show prompt for user's input, assign value to variables
@@ -50,10 +48,8 @@ public class UserService {
 		String passwordInput = inputScanner.nextLine();
 		//close scanner, pass the input into other method for validation
 		inputScanner.close();
-		//================END input from the USER================
-				
-				
-		
+		//================END input from the USER================				
+						
 		String[] stringArray3 = new String[12];
 		//read file to String Array				
 		try { //read the file
@@ -72,48 +68,48 @@ public class UserService {
 				}
 			
 			//modify the output from data.txt and put it into an array, with delimiter ","
-			for (String textLine2: stringArray) {
+			//for (String textLine2: stringArray) {
+			for (int k = 0; k < stringArray3.length; k++) {
 				
+				for (String textLine2: stringArray) {
 				stringArray2 = textLine2.split(",");
-				//System.out.println(" textLine2: "+" "+textLine2);
-				for (int i2 = 0; i2 < stringArray2.length; i2++) {
-					 
-					 for (String textLine4: stringArray2) {
-						 stringArray3[i2] = textLine4;	 
-					 }
-				}
-				
-				
-				for (String textLine3: stringArray2) {					
-					System.out.println(" textLine3 and stringArray2: "+" "+textLine3);
-					
-				}							
 				
 
-			}					
-			//feedback for my troubleshooting
-			System.out.println("string array length: " + stringArray.length);
-			 	
+				for (int j = 0; j < stringArray2.length; j++) { //copy existing elements					
+					//System.out.println("textLine3 and stringArray2: "+" "+textLine3);
+					stringArray3[j] = stringArray2[j];
+				}}			
+				
+				stringArray2 = stringArray;
+				
+				
+								
+			}
+			for (String inputText: stringArray3) {
+				System.out.println("contents stringarray3\n" + inputText);
+			}
+			
 		} catch (FileNotFoundException e) {
 			System.out.println("Oops, the file wasn't found");
 			e.printStackTrace();			
 		}  catch(IOException e){
 			System.out.println("Oops, there was an IO exception");
 		}
-			finally {
-		
-			try{
-				System.out.println("Closing file reader");
-				fileReader.close();
-			} catch (IOException e){
-				System.out.println("Oops! An IOException has occurred");
-				e.printStackTrace();
-			}
+			finally {	
+				try{
+					System.out.println("Closing file reader");
+					fileReader.close();
+				} catch (IOException e){
+						System.out.println("Oops! An IOException has occurred");
+						e.printStackTrace();
+				}
 		}		
 		System.out.println("stringArray length: " + stringArray.length); 				//feedback for my troubleshooting
 		System.out.println("stringArray2 length: " + stringArray2.length);
 		System.out.println("stringArray3 length: " + stringArray3.length);
-		System.out.println("SA3 Index Value Check: " + stringArray3[1]);
+		System.out.println("SA3 Index Value Check: " + stringArray3[6]);
+		System.out.println("SA3 Index Value Check: " + stringArray3[0]);
+		System.out.println("userArray Index Value Check: " + userArray);
 		//input users into the array after parsing					
 		//String[] newArray = parseText(stringArray);		
 		
@@ -122,7 +118,7 @@ public class UserService {
 		//send user input for validation
 		//appInputCheck(usernameInput, passwordInput); 
 	}	
-	
+	//========================================================
 	public static String[] parseText(String[] inputs) {
 		//this method will move elements from all being on the same line and split them onto their own
 		String[] stringArray = null;	
@@ -158,12 +154,9 @@ public class UserService {
 //				System.out.println("userArray contents: " + userArray[i]);
 			//}
 
-			
-		
-
 			return stringArray;
 		}				
-	
+	//========================================================
 	public static void appInputCheck(String username, String password) {
 		//initialize objects
 
