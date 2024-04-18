90086a2639f64d9adeb3d7837a7ace6b000950fd
theTurnedPage <158675598+theTurnedPage@users.noreply.github.com>
2024-04-18 11:24:32 -0500

    Trying to figure out why the array isn't populating correctly

--- src/com/coderscampus/unit5assignment/UserService.java
+++ src/com/coderscampus/unit5assignment/UserService.java
@@ -33,7 +33,8 @@ public class UserService {
 		
 		//initialize objects
 		String[] stringArray = new String[4]; //set to 4 since data.txt has 4 lines
-		System.out.println("string array length: " + stringArray.length);
+		String[] stringArray2 = new String[12];
+		
 		User[] userArray = new User[12]; //take as string then convert to object User
 		User user = new User();
 		Scanner inputScanner = new Scanner(System.in);		
@@ -53,21 +54,44 @@ public class UserService {
 				
 				
 		
-		
+		String[] stringArray3 = new String[12];
 		//read file to String Array				
 		try { //read the file
 			fileReader = new BufferedReader(new FileReader("data.txt")); //had to create data.txt outside src folder 				
 			//start at index 0 for the array
 			int i = 0;
-			String line = "";
+			String textLine = "";
 			
-			//read the file until an empty (null) line is reached
-			while ((line = fileReader.readLine()) != null) {							
+			//read the file until an empty (null) line is reached into stringArray
+			while ((textLine = fileReader.readLine()) != null) {							
 				//insert data into the array
-				stringArray[i] = line.toString();				
+				stringArray[i] = textLine.toString();
+				//feedback for my troubleshooting
 				System.out.println("Line at index " + i + ": " + stringArray[i].toString());				
-				i++;								
-			}								
+				i++;
+				}
+			
+			//modify the output from data.txt and put it into an array, with delimiter ","
+			for (String textLine2: stringArray) {
+				
+				stringArray2 = textLine2.split(",");
+				//System.out.println(" textLine2: "+" "+textLine2);
+				for (int i2 = 0; i2 < stringArray2.length; i2++) {
+					 
+					 for (String textLine4: stringArray2) {
+						 stringArray3[i2] = textLine4;	 
+					 }
+				}
+				
+				
+				for (String textLine3: stringArray2) {					
+					System.out.println(" textLine3 and stringArray2: "+" "+textLine3);
+					
+				}							
+				
+
+			}					
+			//feedback for my troubleshooting
 			System.out.println("string array length: " + stringArray.length);
 			 	
 		} catch (FileNotFoundException e) {
@@ -86,82 +110,63 @@ public class UserService {
 				e.printStackTrace();
 			}
 		}		
-		
-		System.out.println("string array length: " + stringArray.length);
+		System.out.println("stringArray length: " + stringArray.length); 				//feedback for my troubleshooting
+		System.out.println("stringArray2 length: " + stringArray2.length);
+		System.out.println("stringArray3 length: " + stringArray3.length);
+		System.out.println("SA3 Index Value Check: " + stringArray3[1]);
 		//input users into the array after parsing					
-		String[] newArray = parseText(stringArray);
-		System.out.println(newArray.toString());
+		//String[] newArray = parseText(stringArray);		
 		
-		//userArray[1] = userService.createUser(null, null, null);
-		//appInputCheck(usernameInput, passwordInput);
+		//somewhere load up an array of users userArray[i] = userService.createUser(null, null, null);
+		
+		//send user input for validation
+		//appInputCheck(usernameInput, passwordInput); 
 	}	
 	
 	public static String[] parseText(String[] inputs) {
 		//this method will move elements from all being on the same line and split them onto their own
-		//String[] userArray = new String [inputs.length];	
+		String[] stringArray = null;	
 		
-		String[] userArray = inputs.clone();
+		//String[] userArray = inputs.clone();		
 		
-		System.out.println("inputs length: " + inputs.length);
-		System.out.println("userArray length: " + userArray.length);
-		
+		//System.out.println("inputs length: " + inputs.length); 				//feedback for my troubleshooting
+		//System.out.println("userArray length: " + userArray.length);				//feedback for my troubleshooting
+		//modify the output from data.txt and put it into an array, with delimiter ","
+		for (String textLine2: inputs) {
+			System.out.println(" textLine2: "+" "+textLine2);
+			stringArray = textLine2.split(",");
+			
+			for (String textLine3: stringArray) {
+				System.out.println(" textLine3: "+" "+textLine3);
+			}							
+
+		}		
 		
 			for (String input : inputs) {
 				System.out.println("test");
-				//System.out.println(input);
-				//1,2,3,4,5, -> [0]="1". [1]="2", etc.
+
 				String[] splitArray = input.split(",");
 				
 				for (String splits : splitArray) {
-					userArray = splitArray;
+					
 				}
 				
 			}			
 			
-			
-			for (int i = 0; i < userArray.length; i++) {
-				System.out.println(userArray[i]);
-			}
-			
-		 
-
-
-			//System.out.println(userArray[i]);			
-			//userArray[i] = splitArray[i];
-			
-			//System.out.println("split string at index: " + i + " " + splitString[i].toString());
-			//userArray[i] = splitString[i]; 						
-			
-			//for (String data: userArray) {
-				//System.out.println("for loop 2 userArray: " + data);
-				
+			//feedback for my troubleshooting
+			//for (int i = 0; i < userArray.length; i++) {
+//				System.out.println("userArray contents: " + userArray[i]);
 			//}
-			
-		
-		System.out.println("userArray length: " + userArray.length);
-		int i = 0;
-		while(i < userArray.length) {
-			
-			if (i == userArray.length) {
-				break;
-			}
-			
-			System.out.println("userArray at index: " + i + " " +  userArray[i]);// + " " + userArray[i].toString()); //show data we got, success!
-			i++;
 
-		}
+			
 		
-		
-		return userArray;
-	}
+
+			return stringArray;
+		}				
 	
 	public static void appInputCheck(String username, String password) {
 		//initialize objects
-		String[] realUsers = new String[4]; //set to 4 since data.txt has 4 lines
-		User[] realUsers2 = new User[4]; //take as string then convert to object User
-		
-		BufferedReader fileReader = null;
-		UserService userService = new UserService();
+
 		
 		//for each loop? Maybe do it in the try block instead
 		
