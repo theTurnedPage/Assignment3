6bf002023b485755a9588c150f015a8c3e3fc200
theTurnedPage <158675598+theTurnedPage@users.noreply.github.com>
2024-04-18 21:05:28 -0500

    Getting closer, fixing up on validation for login info
    Fixed some duplicates found later in an array. Now trying to fix the detection for a correct login.

--- src/com/coderscampus/unit5assignment/UserService.java
+++ src/com/coderscampus/unit5assignment/UserService.java
@@ -58,23 +58,25 @@ public class UserService {
 		//check user input against array of Users then
 		//give feedback against login creds
 		int i = 0;
-		while (User.loginAttemptsBeforeLock != 0) {
+		while (User.loginAttemptsBeforeLock > 1) {
 					
-		while (i < 4) {
-			if (username == realUsers[i].getUsername() && password == realUsers[i].getPassword()) {
+		while (i < 4) { //username isn't case sensitive, password is
+			if (username.equalsIgnoreCase(realUsers[i].getUsername()) && password == realUsers[i].getPassword()) {
 				System.out.println("Welcome: " + realUsers[i].getName());
 			} else {
-				System.out.println("Invalid login, please try again.");
+				System.out.println("Invalid login, please try again.");				
 				User.loginAttemptsBeforeLock--; //lower login attempts
+				System.out.println("You have " + User.loginAttemptsBeforeLock + " attempts remaining");
 				appInput(); //return user to login again
 			}						
 			i++;
-		}}				
-		
-		if (User.loginAttemptsBeforeLock == 0) {
-			lockoutScreen();						
 		}
-					
+		}				
+			//yeah, I know this is a little lazy . . . 
+		if (User.loginAttemptsBeforeLock == 1 || User.loginAttemptsBeforeLock == 0) {
+			lockoutScreen(); 	
+			System.exit(0); //force app to terminate
+		}					
 	}
 	//========================================================
 	public static void lockoutScreen() {
@@ -96,17 +98,17 @@ public class UserService {
 		try {
 			fileReader = new BufferedReader(new FileReader("data.txt"));
 			
-			while ((aLine = fileReader.readLine()) != null) {				
+							
 										 				
 				for (int i = 0; i < 4; i++) {
-					int j = 0;
+					
 					//System.out.println(k); this shows increments of 3 for k, while loop
 					//runs up to length of splitArray, which is 3
 					//3 * 4 == 12, matching length of newArray
-					
-					
-					newArray = aLine.split(",");
-					
+					while ((aLine = fileReader.readLine()) != null) {
+				
+					splitArray = aLine.split(",");
+					int j = 0;
 					//add the 3 elements into a newArray from splitArray 
 					while (j < splitArray.length && k < newArray.length){
 						newArray[k] = splitArray[j];	
@@ -141,83 +143,15 @@ public class UserService {
 			e.printStackTrace();
 		}
 		//usernames at 0, 3, 6, 9 therefor jump 3 places at a time
-		for (int i = 0; i < 9; i+=3)
+		for (int i = 0; i < 12; i+=3)
 		{	//username password name
-			while(j < 4) {
+			//while(j < 4) {
 				UserService userService = new UserService();
 				userArray[j] = userService.createUser(stringArray[i], stringArray[i+1], stringArray[i+2]);
 				j+=1;
-			}			
+			//}			
 		}			
 	return userArray;			
 	}
 	//========================================================
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	//========================================================
-	public static void testMethod() {
-		
-		String[] newArray = new String[12];
-		String[] splitArray = new String[12];
-		
-		File textFile = new File("data.txt");
-		Scanner fileScanner = new Scanner(System.in);
-		int k = 0;
-		
-		try {
-			fileScanner = new Scanner(textFile);			
-			while(fileScanner.hasNextLine()) {
-						
-				for (int i = 0; i < 4; i++) {
-					int j = 0;
-					//System.out.println(k); this shows increments of 3 for k, while loop
-					//runs up to length of splitArray, which is 3
-					//3 * 4 == 12, matching length of newArray
-					
-					String initialText = fileScanner.nextLine();
-					splitArray = initialText.split(",");
-					
-					//add the 3 elements into a newArray from splitArray 
-					while (j < splitArray.length && k < newArray.length){
-						newArray[k] = splitArray[j];	
-						j++;
-						k++;
-					}
-				}
-									
-				for (String splitText : newArray) {
-									
-					System.out.println(" ForEachLoop Output:\n " + splitText);
-					 
-					//this sysout goes out of bounds whenever newArray = splitArray
-					//and whenever I go past index 2 for splitArray (e.g. 3)
-					//System.out.println("Value at Array Index " + splitArray[3]);
-				}												
-			}
-			fileScanner.close();
-		} catch (FileNotFoundException e) {
-			System.out.println("Oops, a File  Not Found Exception Occurred!");
-			
-			fileScanner.close();
-			e.printStackTrace();
-		}	
-		
-	}
-		
 }
